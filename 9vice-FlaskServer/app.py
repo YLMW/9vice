c3210537db55dc6cfd67bc2961f098dac98bd158
Lulu <lucaslebret.scolaire@gmail.com>
2022-03-18 23:19:11 -0400

    Juste pour tester d'envoyer un message depuis le serveur On fait une loop côté client pour check que le sid ne change pas

--- 9vice-FlaskServer/app.py
+++ 9vice-FlaskServer/app.py
@@ -2,6 +2,9 @@
 # Import des libs python
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flask_socketio import send, emit # Pour utiliser send et emit => réponse du serveur
+from flask_socketio import join_room, leave_room    # Les rooms
+from flask import request   # request.sid
 
 # Import des blueprints de routage
 from routes.main import main
@@ -24,10 +27,11 @@ app.register_blueprint(auth)
 def not_found(error):
     return render_template('pageNotFound.html'), 404
 
-
 @socketio.on('my event')
 def handle_my_custom_namespace_event(json):
     print('received json: ' + str(json))
+    print('Request.sid: ' + request.sid)
+    emit('my answer', request.sid)
 
 # Main
 if __name__ == '__main__':
